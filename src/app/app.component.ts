24f034f1c69e752b7aaf6a4037ae5859abbd1676
yvesrafaralahy <32330334+yvesrafaralahy@users.noreply.github.com>
2017-11-29 15:30:42 +0100

    premier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
 
+class Personne{
+	nom : string;
+	prenom : string;
+	description : string;
+	description : string;
+
+	constructor (nom : string, prenom : string, description : string, travail : string){
+		this.nom = nom;
+		this.prenom = prenom;
+		this.description = description;
+		this.travail = travail;
+	}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+     count = 0;
+
+  title = 'My project:';
+   personne = new Personne("Rafaralahy",
+				"Yves",
+				"Cooking things...",
+				"'if you don't know, don't ask! Don't hate the player, hate the GAME!'");
+
+
+   clickOn(){
+   	this.count++:
+   	this.personne.prenom=this.personne.prenom +" "+this.count;
+   }
+
+  
+ /*
+  import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Liste des Scientifiques';
+  
+  
+  personne = {
+				nom :"Einstein",
+				prenom : "Albert",
+				description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.",
+				travail : "Relativité Générale"
+			};	
+}
+  */
 }
