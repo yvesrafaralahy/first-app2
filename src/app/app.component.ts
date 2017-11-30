8f583992212b3b37bfce6be24ca71b8127e4cd43
yvesrafaralahy <32330334+yvesrafaralahy@users.noreply.github.com>
2017-11-30 11:58:02 +0100

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ class Personne{
 	nom : string;
 	prenom : string;
 	description : string;
-	description : string;
+	travail : string;
 
 	constructor (nom : string, prenom : string, description : string, travail : string){
 		this.nom = nom;
@@ -21,25 +21,36 @@ class Personne{
 })
 export class AppComponent {
 
- 
+  title = 'Liste des "Scientifiques":';
 
-  title = 'My project:';
-   personne = new Personne("Rafaralahy",
+   /*personne = new Personne("Rafaralahy",
 				"Yves",
 				"Cooking things...",
-				"'if you don't know, don't ask! Don't hate the player, hate the GAME!'");
+				"'if you don't know, don't ask! Don't hate the player, hate the GAME!'");*/
+
+   liste =[{nom :"Einstein", prenom : "Albert", description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.", travail : "Relativité Générale"},
+      {nom :"Faraday", prenom : "Michael", description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.", travail : "Relativité Générale"},
+      {nom :"Curie", prenom : "Marie", description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.", travail : "Relativité Générale"},
+          {nom :"Curie", prenom : "Pierre", description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.", travail : "Relativité Générale"}];
 
 
-   clickOn(){
-   	this.count++:
-   	this.personne.prenom=this.personne.prenom +" "+this.count;
+     personne = this.liste[0];  
+
+   clickOn(p){
+   	this.personne = p;
    }
-
+}
+/*
    count = 0;
    output="";
-   calcOn(number){
+
+   calcOn(number:String){
    	this.output=this.output+number;
    }
+*/
+
+
+
 
   
  /*
@@ -55,12 +66,6 @@ export class AppComponent {
   title = 'Liste des Scientifiques';
   
   
-  personne = {
-				nom :"Einstein",
-				prenom : "Albert",
-				description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.",
-				travail : "Relativité Générale"
-			};	
+  
 }
   */
-}
